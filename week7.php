40819bc582877ae269b9e131ebbef8fc306ff705
Dorothy Chau <dorothychauu@gmail.com>
2017-10-18 20:16:31 -0400

    umbreon is the best eevee

--- week7.php
+++ week7.php
@@ -8,9 +8,30 @@
 	try {
     $conn = new PDO($dsn, $username, $password);
     echo "Connected successfully<br>";
+    
+    $query = 'SELECT * FROM accounts
+    		WHERE id <6';
+    $anything = $conn->prepare($query);
+    $anything->execute();
+
+    $product = $anything->fetchAll();
+    $anything->closeCursor();
+
 	} catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
 $conn = null;
 
 ?>
+<html>
+Results: 
+	<?php echo count($product);?> <br></br>
+		<table>
+		<?php foreach ($product as $product) { ?>
+			<tr>
+				<td><?php echo $product['id']; ?></td>
+				<td><?php echo $product['email']; ?></td>
+			</tr>
+		<?php } ?>
+		</table>
+</html>
